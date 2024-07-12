13c0007c3d3df7ee740f09f0ada34eb3bca161ed
Cyrus Chan <cyruschan1212@gmail.com>
2024-07-13 00:28:16 +0800

    No theme setting

--- superuser/example/lib/main.dart
+++ superuser/example/lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:superuser/superuser.dart';
 
-import 'style.dart';
-
 void main() {
-  runApp(ChangeNotifierProvider(
-      create: (context) => ThemePreference(), child: const App()));
+  runApp(const App());
+}
+
+String get _appTitle {
+  String baseTitle = "Superuser demo";
+
+  if (Superuser.isActivated) {
+    baseTitle += " (run as superuser)";
+  }
+
+  return baseTitle;
 }
 
 class App extends StatelessWidget {
@@ -14,14 +20,18 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final ThemePreference pref = context.watch<ThemePreference>();
-
     return MaterialApp(
-        title: "Superuser demo", themeMode: pref.mode, home: const Context());
+        title: _appTitle, home: const Context());
   }
 }
 
 class Context extends StatelessWidget {
+  static const TextStyle _titleStyle =
+      TextStyle(fontSize: 24, fontWeight: FontWeight.w400);
+
+  static const TextStyle _valueStyle =
+      TextStyle(fontSize: 18, fontWeight: FontWeight.w500);
+
   const Context({super.key});
 
   @override
@@ -34,34 +44,23 @@ class Context extends StatelessWidget {
               decoration:
                   BoxDecoration(color: Theme.of(context).secondaryHeaderColor),
               child: null),
-          ListTile(
-              leading: const Icon(Icons.palette),
-              title: const Text("Theme mode"),
-              onTap: () async {
-                ThemeMode? newThemeMode = await showDialog<ThemeMode>(
-                    context: context,
-                    barrierDismissible: false,
-                    builder: (context) => const ThemeModeDialog());
-
-                if (context.mounted && newThemeMode != null) {
-                  context.watch<ThemePreference>().mode = newThemeMode;
-                }
-              })
+          
         ])),
         body: ListView(
             padding: const EdgeInsets.only(top: 8, left: 18, right: 18),
             shrinkWrap: true,
             children: <Widget>[
               ListTile(
-                  title: const Text("Username"),
-                  trailing: Text(Superuser.whoAmI)),
+                  title: const Text("Username", style: _titleStyle),
+                  trailing: Text(Superuser.whoAmI, style: _valueStyle)),
               ListTile(
-                  title: const Text("Has superuser role"),
-                  trailing: Text(Superuser.isSuperuser ? "Yes" : "No")),
+                  title: const Text("Has superuser role", style: _titleStyle),
+                  trailing: Text(Superuser.isSuperuser ? "Yes" : "No",
+                      style: _valueStyle)),
               ListTile(
-                  title: const Text("Run as superuser"),
-                  trailing: Text(Superuser.isActivated ? "Yes" : "No"))
+                  title: const Text("Run as superuser", style: _titleStyle),
+                  trailing: Text(Superuser.isActivated ? "Yes" : "No",
+                      style: _valueStyle))
             ]));
   }
 }
-
