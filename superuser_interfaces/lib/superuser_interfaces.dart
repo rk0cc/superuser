30d5f8ca09e5dc9a984e38194c89980c65fa6800
Cyrus Chan <cyruschan1212@gmail.com>
2024-07-12 18:57:34 +0800

    Format

--- superuser_interfaces/lib/superuser_interfaces.dart
+++ superuser_interfaces/lib/superuser_interfaces.dart
@@ -6,13 +6,13 @@ abstract interface class SuperuserInterface {
   const SuperuserInterface._();
 
   /// Determine current user has superuser role.
-  /// 
+  ///
   /// To determine Flutter program executed with
   /// superuser right, consider using [isActivated].
   bool get isSuperuser;
 
   /// Determine this program is executed with superuser right.
-  /// 
+  ///
   /// This can be `true` only if [isSuperuser] `true` at the
   /// same time.
   bool get isActivated;
@@ -23,9 +23,9 @@ abstract interface class SuperuserInterface {
 
 /// Replicate behaviour of [SuperuserInterface], which
 /// fulfilled requirements of testing.
-/// 
+///
 /// It is ideal for widget testing that it can simulate
-/// superuser status without 
+/// superuser status without
 /// [Run as administrator](https://learn.microsoft.com/en-us/troubleshoot/windows-server/shell-experience/use-run-as-start-app-admin)
 /// or [`sudo` command](https://man7.org/linux/man-pages/man8/sudo.8.html).
 final class MockSuperuser implements SuperuserInterface {
@@ -40,12 +40,10 @@ final class MockSuperuser implements SuperuserInterface {
 
   /// Create mocked properties of [SuperuserInterface] to emulate
   /// superuser status.
-  /// 
-  /// It is forbidden to enable [isActivated] without [isSuperuser]. 
+  ///
+  /// It is forbidden to enable [isActivated] without [isSuperuser].
   MockSuperuser(
-      {this.isSuperuser = false,
-      this.isActivated = false,
-      this.whoAmI = ""})
+      {this.isSuperuser = false, this.isActivated = false, this.whoAmI = ""})
       : assert(() {
           if (!isSuperuser) {
             return !isActivated;
