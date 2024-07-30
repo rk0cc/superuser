aab279656de2911f6dc2b6a21f06c6d74a6d5e42
Cyrus Chan <cyruschan1212@gmail.com>
2024-07-30 21:50:32 +0800

    Reimpl binary search for windows

--- superuser_plugin_windows/src/superuser_plugin_windows.c
+++ superuser_plugin_windows/src/superuser_plugin_windows.c
@@ -6,16 +6,16 @@
 
 #define MAX_USERNAME_CHAR 256
 
-BOOL __wchar_to_utf8(WCHAR* wc, char** utf)
+BOOL __wchar_to_utf8(WCHAR *wc, char **utf)
 {
     int buf8_size = WideCharToMultiByte(CP_UTF8, 0, wc, -1, NULL, 0, NULL, NULL);
 
-    *utf = (char *) calloc(buf8_size, sizeof(char));
+    *utf = (char *)calloc(buf8_size, sizeof(char));
 
     return WideCharToMultiByte(CP_UTF8, 0, wc, -1, *utf, buf8_size, NULL, NULL);
 }
 
-ERRCODE __obtain_user_local_group(LPBYTE* gp, DWORD* entries, DWORD* total)
+ERRCODE __obtain_user_local_group(LPBYTE *gp, DWORD *entries, DWORD *total)
 {
     WCHAR ubuf[MAX_USERNAME_CHAR];
     DWORD ubufLen = sizeof(ubuf) / sizeof(ubuf[0]);
@@ -33,8 +33,18 @@ ERRCODE __obtain_user_local_group(LPBYTE* gp, DWORD* entries, DWORD* total)
     return 0;
 }
 
+int __sort_search_lguser(void *context, const void *a, const void *b)
+{
+    LPCWCHAR aChar, bChar;
+
+    aChar = (*(LOCALGROUP_USERS_INFO_0 *)a).lgrui0_name;
+    bChar = (*(LOCALGROUP_USERS_INFO_0 *)b).lgrui0_name;
+
+    return wcscmp(aChar, bChar);
+}
+
 // Verify user who execute program has admin right.
-FFI_PLUGIN_EXPORT ERRCODE is_admin_user(bool* result)
+FFI_PLUGIN_EXPORT ERRCODE is_admin_user(bool *result)
 {
     LPBYTE buf;
     DWORD entries, total;
@@ -43,19 +53,17 @@ FFI_PLUGIN_EXPORT ERRCODE is_admin_user(bool* result)
     if (err)
         return err;
 
-    LPCWSTR adminGpName = L"Administrators";
-    LOCALGROUP_USERS_INFO_0* lg = (LOCALGROUP_USERS_INFO_0*) buf;
-    
-    bool tmp_result = false;
-    for (DWORD i = 0; i < entries; i++)
-    {
-        if (wcscmp(adminGpName, lg[i].lgrui0_name) == 0)
-        {
-            tmp_result = true;
-            break;
-        }
-    }
+    LOCALGROUP_USERS_INFO_0 key = {.lgrui0_name = L"Administrators"};
+    LOCALGROUP_USERS_INFO_0 *lg = (LOCALGROUP_USERS_INFO_0 *)buf;
 
+    errno = 0;
+    qsort_s(lg, entries, sizeof(LOCALGROUP_USERS_INFO_0), __sort_search_lguser, NULL);
+    LOCALGROUP_USERS_INFO_0 *found = (LOCALGROUP_USERS_INFO_0 *)bsearch_s(&key, lg, entries, sizeof(LOCALGROUP_USERS_INFO_0), __sort_search_lguser, NULL);
+
+    if (errno > 0)
+        return errno;
+
+    bool tmp_result = found != NULL;
     *result = tmp_result;
 
     NetApiBufferFree(buf);
@@ -64,14 +72,14 @@ FFI_PLUGIN_EXPORT ERRCODE is_admin_user(bool* result)
 }
 
 // Determine this program is executed with admin.
-FFI_PLUGIN_EXPORT ERRCODE is_elevated(bool* result)
+FFI_PLUGIN_EXPORT ERRCODE is_elevated(bool *result)
 {
     HANDLE token;
 
     SetLastError(0);
     if (!OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &token))
         return GetLastError();
-    
+
     TOKEN_ELEVATION elevation;
     DWORD cbSize = sizeof(TOKEN_ELEVATION);
 
@@ -90,7 +98,7 @@ FFI_PLUGIN_EXPORT ERRCODE is_elevated(bool* result)
 }
 
 // Obtain name of user.
-FFI_PLUGIN_EXPORT ERRCODE get_current_username(char** result)
+FFI_PLUGIN_EXPORT ERRCODE get_current_username(char **result)
 {
     WCHAR buffer[MAX_USERNAME_CHAR];
     DWORD bufLen = sizeof(buffer) / sizeof(buffer[0]);
@@ -107,7 +115,7 @@ FFI_PLUGIN_EXPORT ERRCODE get_current_username(char** result)
 }
 
 // Obtain user's associated group in local system.
-FFI_PLUGIN_EXPORT ERRCODE get_associated_groups(char*** groups, DWORD* length)
+FFI_PLUGIN_EXPORT ERRCODE get_associated_groups(char ***groups, DWORD *length)
 {
     LPBYTE buf;
     DWORD entries, total;
@@ -116,9 +124,9 @@ FFI_PLUGIN_EXPORT ERRCODE get_associated_groups(char*** groups, DWORD* length)
     if (err)
         return err;
 
-    char** tmp_groups = (char**) calloc(entries, sizeof(char*));
+    char **tmp_groups = (char **)calloc(entries, sizeof(char *));
 
-    LOCALGROUP_USERS_INFO_0* lg = (LOCALGROUP_USERS_INFO_0*) buf;
+    LOCALGROUP_USERS_INFO_0 *lg = (LOCALGROUP_USERS_INFO_0 *)buf;
     for (DWORD i = 0; i < entries; i++)
     {
         if (!__wchar_to_utf8(lg[i].lgrui0_name, &tmp_groups[i]))
@@ -134,7 +142,7 @@ FFI_PLUGIN_EXPORT ERRCODE get_associated_groups(char*** groups, DWORD* length)
 }
 
 // Flush dynamic allocated function.
-FFI_PLUGIN_EXPORT void flush(void* ptr)
+FFI_PLUGIN_EXPORT void flush(void *ptr)
 {
     free(ptr);
 }
