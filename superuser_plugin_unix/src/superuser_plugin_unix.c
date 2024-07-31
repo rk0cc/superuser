589d6e6b056358055752a231feba951cd8d16a6d
Cyrus Chan <cyruschan1212@gmail.com>
2024-07-31 10:57:07 +0800

    Remove unused enum

--- superuser_plugin_unix/src/superuser_plugin_unix.c
+++ superuser_plugin_unix/src/superuser_plugin_unix.c
@@ -22,8 +22,7 @@ typedef enum _error_causes
     pwuid_err = 1,
     grnam_err,
     grouplist_err,
-    grgid_err,
-    qso_bse_err
+    grgid_err
 } error_causes;
 
 static ERRCODE __build_suunix_error_code(error_causes causes, int error_code)
@@ -140,7 +139,7 @@ FFI_PLUGIN_EXPORT ERRCODE is_sudo_group(bool *result)
 
     if (gp_list_err > 0)
         return gp_list_err;
-        
+
     qsort(gp_lists, ngps, sizeof(gid_t), __sort_search_gid_compare);
     gid_t *found = (gid_t *)bsearch(&sudo_gpid, gp_lists, ngps, sizeof(gid_t), __sort_search_gid_compare);
 
