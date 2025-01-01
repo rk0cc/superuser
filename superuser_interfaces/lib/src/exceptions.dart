3192db3b27941b71046cb4cbacd4683e800703bc
Cyrus Chan <cyruschan1212@gmail.com>
2025-01-01 20:55:16 +0800

    Construct detailed exceptions

--- superuser_interfaces/lib/src/exceptions.dart
+++ superuser_interfaces/lib/src/exceptions.dart
@@ -1,21 +1,40 @@
 import 'dart:io';
 
+/// Detailed information from [SuperuserProcessError] to indicate
+/// the result returns with error in FFI.
+final class ErrorDetail {
+  /// Error code from FFI methods.
+  final int code;
+
+  /// Name of API method, which cause this error occured.
+  final String apiMethodName;
+
+  /// Name of getter of the [code].
+  final String errGetterName;
+
+  /// Create detailed error information from FFI.
+  const ErrorDetail(this.code, this.apiMethodName, this.errGetterName);
+}
+
 /// Indicate any errors encountered when fetching properties
 /// in [SuperuserInterface].
 class SuperuserProcessError extends Error implements OSError {
-  /// Error code returned from native library.
-  @override
-  final int errorCode;
+  /// A structured information that containing how this error
+  /// throw when executing FFI.
+  final ErrorDetail detail;
 
-  /// Message to explain [errorCode].
+  /// Message to explain based on [detail].
   @override
   final String message;
 
-  /// Create [SuperuserProcessError] with given [errorCode].
+  /// Create [SuperuserProcessError] with given error [detail].
   ///
   /// Optionally, provide a [message] for further explaination
   /// of error.
-  SuperuserProcessError(this.errorCode, [this.message = ""]);
+  SuperuserProcessError(this.detail, [this.message = ""]);
+
+  @override
+  int get errorCode => detail.code;
 
   @override
   String toString() {
@@ -26,10 +45,14 @@ class SuperuserProcessError extends Error implements OSError {
     if (message.isNotEmpty) {
       buf
         ..write(message)
-        ..write(" (error code: $errorCode)");
+        ..writeln(" (error code: $errorCode)");
     } else {
-      buf.write("process return with error code $errorCode");
+      buf.writeln("process return with error code $errorCode");
     }
+
+    buf
+      ..writeln("Method name: ${detail.apiMethodName}")
+      ..writeln("Error getter: ${detail.errGetterName}");
 
     return buf.toString();
   }
