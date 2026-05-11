67278f85affde0a12c1f2dc610ab7c4d39fc87c0
Cyrus Chan <cyruschan1212@gmail.com>
2026-05-11 21:39:31 +0800

    New API and group extract approach

--- superuser_plugin_windows/src/superuser_plugin_windows.c
+++ superuser_plugin_windows/src/superuser_plugin_windows.c
@@ -7,28 +7,6 @@
     SetLastError(0);      \
     BOOL
 
-void __get_current_username(SUPERUSER_ERRORINFO *errinfo, LPWSTR *uname)
-{
-    WCHAR ubuf[MAX_USERNAME_CHAR];
-    DWORD ubufLen = sizeof(ubuf) / sizeof(ubuf[0]);
-
-    WINAPICALL_STATUS success = GetUserNameW(ubuf, &ubufLen);
-    if (!success)
-    {
-        errinfo->code = GetLastError();
-        wcscpy_s(errinfo->winapi_func_name, WIN32API_FUNC_WLEN, L"GetUserNameW (__get_current_username)");
-
-        return;
-    }
-
-    errno_t cpy_errno = wcscpy_s(*uname, MAX_USERNAME_CHAR, ubuf);
-    if (cpy_errno != ERROR_SUCCESS)
-    {
-        errinfo->code = cpy_errno;
-        wcscpy_s(errinfo->winapi_func_name, WIN32API_FUNC_WLEN, L"wcscpy_s (__get_current_username)");
-    }
-}
-
 void __open_handle_token(SUPERUSER_ERRORINFO *errinfo, HANDLE *hToken)
 {
     WINAPICALL_STATUS opened = OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, hToken);
@@ -43,7 +21,7 @@ void __open_handle_token(SUPERUSER_ERRORINFO *errinfo, HANDLE *hToken)
 void __get_group_token(SUPERUSER_ERRORINFO *errinfo, HANDLE *hToken, PTOKEN_GROUPS *gpInfo)
 {
     DWORD gpSize;
-    WINAPICALL_STATUS hasBuf = GetTokenInformation(hToken,
+    WINAPICALL_STATUS hasBuf = GetTokenInformation(*hToken,
                                                    TokenGroups,
                                                    NULL,
                                                    0,
@@ -58,8 +36,8 @@ void __get_group_token(SUPERUSER_ERRORINFO *errinfo, HANDLE *hToken, PTOKEN_GROU
     }
 
     SetLastError(0);
-    PTOKEN_GROUPS tmpGpInfo = (PTOKEN_GROUPS)LocalAlloc(LPTR, gpSize);
-    if (!tmpGpInfo)
+    *gpInfo = (PTOKEN_GROUPS)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, gpSize);
+    if (*gpInfo == NULL)
     {
         errinfo->code = GetLastError();
         wcscpy_s(errinfo->winapi_func_name, WIN32API_FUNC_WLEN, L"LocalAlloc (__get_group_token)");
@@ -67,32 +45,57 @@ void __get_group_token(SUPERUSER_ERRORINFO *errinfo, HANDLE *hToken, PTOKEN_GROU
         return;
     }
 
-    WINAPICALL_STATUS gpFetched = GetTokenInformation(hToken,
+    WINAPICALL_STATUS gpFetched = GetTokenInformation(*hToken,
                                                       TokenGroups,
-                                                      tmpGpInfo,
+                                                      *gpInfo,
                                                       gpSize,
                                                       &gpSize);
 
     if (!gpFetched)
     {
-        LocalFree(tmpGpInfo);
+        HeapFree(GetProcessHeap(), 0, (LPVOID)*gpInfo);
 
         errinfo->code = GetLastError();
         wcscpy_s(errinfo->winapi_func_name, WIN32API_FUNC_WLEN, L"GetTokenInformation (__get_group_token, fetch)");
-
-        return;
     }
-
-    gpInfo = &tmpGpInfo;
 }
 
-int __sort_search_group_name(const void *a, const void *b)
+int __compare_sid(const void *a, const void *b)
 {
-    LPCWCHAR aChar, bChar;
+    PSID sid1 = *(PSID *)a, sid2 = *(PSID *)b;
+    DWORD len1 = GetLengthSid(sid1), len2 = GetLengthSid(sid2);
 
-    // TODO: Reimplement
+    // Compare lengths difference
+    int diff = (len1 > len2) - (len1 < len2);
 
-    return wcscmp(aChar, bChar);
+    return diff == 0 ? memcmp(sid1, sid2, len1) : diff;
+}
+
+// Extract machine name in NetBIOS.
+FFI_PLUGIN_EXPORT SUPERUSER_ERRORINFO get_local_machine_name(LPWSTR *result)
+{
+    SUPERUSER_ERRORINFO errinfo = {0};
+    
+    WCHAR comName[NETBIOS_NAME_LEN];
+    DWORD bufSize = sizeof(comName) / sizeof(comName[0]);
+
+    WINAPICALL_STATUS hasName = GetComputerNameW(comName, &bufSize);
+    if (!hasName)
+    {
+        errinfo.code = GetLastError();
+        wcscpy_s(errinfo.winapi_func_name, WIN32API_FUNC_WLEN, L"GetComputerNameW (get_local_machine_name)");
+
+        return errinfo;
+    }
+
+    errno_t cpErr = wcscpy_s(*result, NETBIOS_NAME_LEN, comName);
+    if (cpErr != ERROR_SUCCESS)
+    {
+        errinfo.code = cpErr;
+        wcscpy_s(errinfo.winapi_func_name, WIN32API_FUNC_WLEN, L"wcscpy_s (get_local_machine_name)");
+    }
+
+    return errinfo;
 }
 
 // Obtain name of user.
@@ -100,7 +103,24 @@ FFI_PLUGIN_EXPORT SUPERUSER_ERRORINFO get_current_username(LPWSTR *result)
 {
     SUPERUSER_ERRORINFO errinfo = {0};
 
-    __get_current_username(&errinfo, result);
+    WCHAR ubuf[MAX_USERNAME_CHAR];
+    DWORD ubufLen = sizeof(ubuf) / sizeof(ubuf[0]);
+
+    WINAPICALL_STATUS success = GetUserNameW(ubuf, &ubufLen);
+    if (!success)
+    {
+        errinfo.code = GetLastError();
+        wcscpy_s(errinfo.winapi_func_name, WIN32API_FUNC_WLEN, L"GetUserNameW (__get_current_username)");
+
+        return;
+    }
+
+    errno_t cpy_errno = wcscpy_s(*result, MAX_USERNAME_CHAR, ubuf);
+    if (cpy_errno != ERROR_SUCCESS)
+    {
+        errinfo.code = cpy_errno;
+        wcscpy_s(errinfo.winapi_func_name, WIN32API_FUNC_WLEN, L"wcscpy_s (__get_current_username)");
+    }
 
     return errinfo;
 }
@@ -131,14 +151,14 @@ FFI_PLUGIN_EXPORT SUPERUSER_ERRORINFO count_associated_groups_length(PDWORD leng
 
     *length = gpInfo->GroupCount;
 
-    LocalFree(gpInfo);
+    HeapFree(GetProcessHeap(), 0, (LPVOID)gpInfo);
     CloseHandle(hToken);
 
     return errinfo;
 }
 
 // Obtain user's associated group in local system.
-FFI_PLUGIN_EXPORT SUPERUSER_ERRORINFO get_associated_groups(LPWSTR **groups)
+FFI_PLUGIN_EXPORT SUPERUSER_ERRORINFO get_associated_groups(WINDOWS_GROUP_NAME **groups)
 {
     SUPERUSER_ERRORINFO errinfo = {0};
     HANDLE hToken = NULL;
@@ -161,16 +181,62 @@ FFI_PLUGIN_EXPORT SUPERUSER_ERRORINFO get_associated_groups(LPWSTR **groups)
         return errinfo;
     }
 
-    DWORD offerGroupsSize = sizeof(*groups) / sizeof((*groups)[0]);
-    if (offerGroupsSize < gpInfo->GroupCount)
+    DWORD gpLen = _msize(*groups) / sizeof(WINDOWS_GROUP_NAME);
+    if (gpLen != gpInfo->GroupCount)
     {
-        LocalFree(gpInfo);
+        HeapFree(GetProcessHeap(), 0, (LPVOID)gpInfo);
         CloseHandle(hToken);
 
         errinfo.code = ERROR_INSUFFICIENT_BUFFER;
         wcscpy_s(errinfo.winapi_func_name, WIN32API_FUNC_WLEN, L"<internal> (get_associated_groups)");
 
         return errinfo;
+    }
+
+    for (DWORD cursor = 0; cursor < gpInfo->GroupCount; cursor++)
+    {
+        WCHAR gpName[MAX_USERNAME_CHAR], gpDomain[MAX_USERNAME_CHAR];
+        DWORD nameLen, domainLen;
+        nameLen = domainLen = sizeof(gpName) / sizeof(gpName[0]); // Both fields have the same limit of name length.
+        SID_NAME_USE snu;
+
+        PSID lookupSID = gpInfo->Groups[cursor].Sid;
+        WINAPICALL_STATUS nameClawed = LookupAccountSidW(NULL,
+                                                         lookupSID,
+                                                         gpName,
+                                                         &nameLen,
+                                                         gpDomain,
+                                                         &domainLen,
+                                                         &snu);
+        if (!nameClawed)
+        {
+            HeapFree(GetProcessHeap(), 0, (LPVOID)gpInfo);
+            CloseHandle(hToken);
+
+            errinfo.code = GetLastError();
+            wcscpy_s(errinfo.winapi_func_name, WIN32API_FUNC_WLEN, L"LookupAccountSidW (get_associated_groups)");
+
+            return errinfo;
+        }
+
+        errno_t cpErrs[2] = {0, 0};
+        cpErrs[0] = wcscpy_s((*groups)[cursor].name, MAX_USERNAME_CHAR, gpName);
+        cpErrs[1] = wcscpy_s((*groups)[cursor].domain, MAX_USERNAME_CHAR, gpDomain);
+
+        for (int errScope = 0; errScope < 2; errScope++)
+        {
+            errno_t cpErrResult = cpErrs[errScope];
+            if (cpErrResult != ERROR_SUCCESS)
+            {
+                HeapFree(GetProcessHeap(), 0, (LPVOID)gpInfo);
+                CloseHandle(hToken);
+
+                errinfo.code = cpErrResult;
+                wcscpy_s(errinfo.winapi_func_name, WIN32API_FUNC_WLEN, L"wcscpy_s (get_associated_groups)");
+
+                return errinfo;
+            }
+        }
     }
 
     return errinfo;
@@ -218,10 +284,20 @@ FFI_PLUGIN_EXPORT SUPERUSER_ERRORINFO is_admin_user(bool *result)
 
         CloseHandle(hToken);
 
-        return errinfo;
+        return errinfo; // This sould returns error code 0
     }
 
     /* Fallback approach when the one-shot test yield failed */
+
+    PTOKEN_GROUPS gps;
+    __get_group_token(&errinfo, &hToken, &gps);
+
+    if (errinfo.code != 0)
+    {
+        CloseHandle(hToken);
+
+        return errinfo;
+    }
 
     SID_IDENTIFIER_AUTHORITY ntAuth = SECURITY_NT_AUTHORITY;
     PSID adminGp = NULL;
@@ -234,6 +310,7 @@ FFI_PLUGIN_EXPORT SUPERUSER_ERRORINFO is_admin_user(bool *result)
 
     if (!fallbackAdminInit)
     {
+        HeapFree(GetProcessHeap(), 0, (LPVOID)gps);
         CloseHandle(hToken);
 
         errinfo.code = GetLastError();
@@ -242,17 +319,20 @@ FFI_PLUGIN_EXPORT SUPERUSER_ERRORINFO is_admin_user(bool *result)
         return errinfo;
     }
 
-    WINAPICALL_STATUS fallbackAdminCheck = CheckTokenMembership(NULL,
-                                                                adminGp,
-                                                                result);
-
-    if (!fallbackAdminCheck)
+    bool tmpResult = false;
+    for (DWORD cursor = 0; cursor < gps->GroupCount; cursor++)
     {
-        errinfo.code = GetLastError();
-        wcscpy_s(errinfo.winapi_func_name, WIN32API_FUNC_WLEN, L"CheckTokenMembership (is_admin_user)");
+        PSID lookupSID = gps->Groups[cursor].Sid;
+
+        tmpResult = __compare_sid(&lookupSID, &adminGp) == 0;
+        if (tmpResult)
+            break; // Immediate stop iteration once admin role of this user is verified.
     }
 
+    *result = tmpResult;
+
     FreeSid(adminGp);
+    HeapFree(GetProcessHeap(), 0, (LPVOID)gps);
     CloseHandle(hToken);
     return errinfo;
 }
--- superuser_plugin_windows/src/superuser_plugin_windows.h
+++ superuser_plugin_windows/src/superuser_plugin_windows.h
@@ -1,28 +1,39 @@
 #ifndef FLUTTER_SUPERUSER_WINDOWS_H
 #define FLUTTER_SUPERUSER_WINDOWS_H
 
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ < 202311L
+#include <stdbool.h> // C23 make "bool" type as built-in
+#endif
+
 #ifdef __cplusplus
 extern "C"
 {
 #endif
-#include <stdbool.h>
 #include <windows.h>
 
 #define MAX_USERNAME_CHAR 257
-#define WIN32API_FUNC_WLEN 101
+#define WIN32API_FUNC_WLEN 129
+#define NETBIOS_NAME_LEN 16
 #define FFI_PLUGIN_EXPORT __declspec(dllexport)
 
     // Returned value indicates the process result, which
     // uses non-zero values to denotes problem during processing.
     typedef DWORD ERRCODE;
 
-    struct _SUPERUSER_ERRORINFO
+    typedef struct _SUPERUSER_ERRORINFO
     {
         ERRCODE code;
         WCHAR winapi_func_name[WIN32API_FUNC_WLEN];
-    };
+    } SUPERUSER_ERRORINFO;
 
-    typedef struct _SUPERUSER_ERRORINFO SUPERUSER_ERRORINFO;
+    typedef struct _WINDOWS_GROUP_NAME
+    {
+        WCHAR name[MAX_USERNAME_CHAR];
+        WCHAR domain[MAX_USERNAME_CHAR];
+    } WINDOWS_GROUP_NAME;
+
+    // Extract machine name in NetBIOS.
+    FFI_PLUGIN_EXPORT SUPERUSER_ERRORINFO get_local_machine_name(LPWSTR *result);
 
     // Obtain name of user.
     FFI_PLUGIN_EXPORT SUPERUSER_ERRORINFO get_current_username(LPWSTR *result);
@@ -31,7 +42,7 @@ extern "C"
     FFI_PLUGIN_EXPORT SUPERUSER_ERRORINFO count_associated_groups_length(PDWORD length);
 
     // Obtain user's associated group in local system.
-    FFI_PLUGIN_EXPORT SUPERUSER_ERRORINFO get_associated_groups(LPWSTR **groups);
+    FFI_PLUGIN_EXPORT SUPERUSER_ERRORINFO get_associated_groups(WINDOWS_GROUP_NAME *groups);
 
     // Verify user who execute program has admin right.
     FFI_PLUGIN_EXPORT SUPERUSER_ERRORINFO is_admin_user(bool *result);
