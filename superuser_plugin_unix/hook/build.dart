2288a80cb3739acbe5437f050b886230954d7a00
Cyrus Chan <cyruschan1212@gmail.com>
2026-05-19 18:06:15 +0800

    Modify hook configurations

--- superuser_plugin_unix/hook/build.dart
+++ superuser_plugin_unix/hook/build.dart
@@ -1,20 +1,22 @@
-import 'dart:io';
-
 import 'package:code_assets/code_assets.dart';
 import 'package:hooks/hooks.dart';
 import 'package:native_toolchain_c/native_toolchain_c.dart';
 
 void main(List<String> args) async {
   await build(args, (input, output) async {
-    if (!input.config.buildCodeAssets ||
-        !(Platform.isLinux || Platform.isMacOS)) {
+    final CodeConfig(:targetOS) = input.config.code;
+
+    final bool isUnixForDesktop = <OS>[
+      OS.macOS,
+      OS.linux,
+    ].any((os) => targetOS == os);
+
+    if (!input.config.buildCodeAssets || !isUnixForDesktop) {
       return;
     }
 
-    final CodeConfig(:targetOS, :targetArchitecture) = input.config.code;
-
     final builder = CBuilder.library(
-      name: "superuser_plugin_${targetOS}_$targetArchitecture",
+      name: "superuser_plugin",
       assetName: "src/unix_superuser.g.dart",
       sources: const <String>["src/superuser_plugin_unix.c"],
       std: "c17",
--- superuser_plugin_windows/hook/build.dart
+++ superuser_plugin_windows/hook/build.dart
@@ -1,19 +1,17 @@
-import 'dart:io';
-
 import 'package:code_assets/code_assets.dart';
 import 'package:hooks/hooks.dart';
 import 'package:native_toolchain_c/native_toolchain_c.dart';
 
 void main(List<String> args) async {
   await build(args, (input, output) async {
-    if (!input.config.buildCodeAssets || !Platform.isWindows) {
+    final CodeConfig(:targetOS) = input.config.code;
+
+    if (!input.config.buildCodeAssets || targetOS != OS.windows) {
       return;
     }
 
-    final CodeConfig(:targetArchitecture) = input.config.code;
-
     final builder = CBuilder.library(
-      name: "superuser_plugin_windows_$targetArchitecture",
+      name: "superuser_plugin",
       assetName: "src/win_superuser.g.dart",
       sources: const <String>["src/superuser_plugin_windows.c"],
       std: "c17",
